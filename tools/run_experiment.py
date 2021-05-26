03b25d424b3151715c9aae02cdba11235edbc00e
Cuong Nguyen <ctring23@gmail.com>
2021-05-26 15:11:17 -0400

    Tweaking the experiment configs and scripts

--- tools/run_experiment.py
+++ tools/run_experiment.py
@@ -39,12 +39,12 @@ def generate_config(template_path: str, settings: dict):
     return config_path
 
 
-def cleanup(config, user):
+def cleanup(config, user, image):
     admin.main([
         "benchmark",
         config,
         "--user", user,
-        "--no-pull",
+        "--image", image,
         "--cleanup",
         "--clients", "0",
         "--txns", "0"
@@ -81,10 +81,12 @@ def ycsb(args):
     user = settings['username']
 
     varying_args = ["clients", "txns", "duration"]
-    varying_params = ["writes", "records", "hot_records", "hot", "mp", "mh"]
+    varying_params = [
+        "writes", "records", "hot_records", "mp_parts", "mh_homes", "hot", "mp", "mh"
+    ]
 
     workload_setting = settings["ycsb"]
-    ycsb_out_dir = os.path.join(args.out_dir, "ycsb")
+    ycsb_out_dir = os.path.join(args.out_dir, "ycsb" if args.name is None else args.name)
 
     for server in workload_setting["servers"]:
         config = generate_config(os.path.join(args.config_dir, server['config']), settings)
@@ -93,17 +95,23 @@ def ycsb(args):
         common_args = [config, "--user", user, "--image", image]
 
         LOG.info("Cleaning up previous experiments...")
-        cleanup(config, user)
-
+        cleanup(config, user, image)
+ 
         LOG.info("Starting servers...")
         admin.main(["start", *common_args])
-
+  
         LOG.info("Waiting for all servers to be online...")
         admin.main(["collect_server", *common_args, "--flush-only", "--no-pull"])
 
         # Compute the Cartesian product of all varying values
         varying_keys = varying_args + varying_params
-        varying_values = itertools.product(*[workload_setting[k] for k in varying_keys])
+        ordered_value_lists = []
+        for k in varying_keys:
+            if k not in workload_setting:
+                raise KeyError(f"Missing required key in workload setting: {k}")
+            ordered_value_lists.append(workload_setting[k])
+
+        varying_values = itertools.product(*ordered_value_lists)
         values = [dict(zip(varying_keys, v)) for v in varying_values]
 
         tag_keys = [k for k in varying_keys if len(workload_setting[k]) > 1]
@@ -122,7 +130,9 @@ def ycsb(args):
                 "--sample", "10",
                 "--seed", "0",
                 "--params", params,
-                "--tag", tag
+                "--tag", tag,
+                # The image has already been pulled in the cleanup step
+                "--no-pull"
             ])
 
             time.sleep(2)
@@ -149,6 +159,18 @@ if __name__ == "__main__":
                         help="Name of the experiment to run")
     parser.add_argument("--config-dir", "-c", default="config", help="Path to the configuration files")
     parser.add_argument("--out-dir", "-o", default=".", help="Path to the output directory")
+    parser.add_argument("--name", "-n", help="Override name of the experiment directory")
+    parser.add_argument(
+        "--dry-run",
+        action='store_true',
+        help="Check the settings and generate configs without running the experiment"
+    )
     args = parser.parse_args()
+
+    if args.dry_run:
+        def noop(args):
+            print('\t' + ' '.join(args))
+            print()
+        admin.main = noop
 
     EXPERIMENTS[args.experiment](args)
