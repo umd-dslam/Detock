a5e9bf5877cb2b0d957e6ea547f7c6f704143667
Cuong Nguyen <ctring23@gmail.com>
2022-05-19 00:58:50 -0400

    Rate limiter

---
+++ common/rate_limiter.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <chrono>
+
+namespace slog {
+
+// Limiting request rates to a fixed number of requests per second using
+// the token bucket algorithm
+class RateLimiter {
+  using Clock = std::chrono::steady_clock;
+  using Resolution = std::chrono::nanoseconds;
+
+ public:
+  RateLimiter(int req_per_sec)
+      : time_per_req_(0), time_(0), last_check_(Clock::now()) {
+    if (req_per_sec > 0) {
+      time_per_req_ = Resolution::period::den / req_per_sec;
+    }
+  }
+
+  bool Request() {
+    auto now = Clock::now();
+    time_ += std::chrono::duration_cast<Resolution>(now - last_check_).count();
+    last_check_ = now;
+
+    if (time_ > Resolution::period::den) time_ = Resolution::period::den;
+
+    if (time_ >= time_per_req_) {
+      time_ -= time_per_req_;
+      return true;
+    }
+
+    return false;
+  }
+
+ private:
+  uint64_t time_per_req_;
+  uint64_t time_;
+  std::chrono::time_point<Clock> last_check_;
+};
+
+}  // namespace slog
